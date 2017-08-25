0f25daf88d849feaf5ad3214051a33be3cdc146b
Sean <ussa.say@gmail.com>
2017-08-24 20:46:05 -0700

    Added WIP search bar

--- templates/t01.js
+++ templates/t01.js
@@ -3,8 +3,12 @@ window.addEventListener("load",e=>{ // ensure everything is loaded so I get to m
     // toggles if an element has a class or not
     element.classList.contains(className)?element.classList.remove(className):element.classList.add(className);
   }
-  var navMenuBtn=document.querySelector('.navigation .menu'); // get references to HTML elements
-  navMenuBtn.addEventListener("click",e=>{
+  var navMenuBtn=document.querySelector('.navigation > .menu'), // get references to HTML elements
+  searchBtn=document.querySelector('.navigation > .search');
+  navMenuBtn.addEventListener("click",e=>{ // toggle a class for the <body> element when the element is pressed
     classToggle(document.body,'showNavLinks');
+  },false);
+  searchBtn.addEventListener("click",e=>{
+    classToggle(document.body,'showSearch');
   },false);
 },false);
